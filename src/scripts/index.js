296b559fd00313a207e6dbe2ca07c39bdeb9b103
Rian Febriansyah <rianfebriansyah22@gmail.com>
2021-05-12 21:54:00 +0700

    Change id name of button, drawer, and content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,11 @@
-import "regenerator-runtime"; /* for async await transpile */
-import "../styles/style.scss";
-import "./hamburger";
-import "./restaurant";
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/style.scss';
+// import "./hamburger";
+import App from './views/app';
+import './data/restaurant';
+
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#drawerElement'),
+  content: document.querySelector('#mainContent'),
+});
