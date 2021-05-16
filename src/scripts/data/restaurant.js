4b87c318d79eec0aea33055fafa467c1658c80cc
Rian Febriansyah <rianfebriansyah22@gmail.com>
2021-05-16 20:11:20 +0700

    Applying code convention

--- src/scripts/data/restaurant.js
+++ src/scripts/data/restaurant.js
@@ -3,22 +3,22 @@ const data = require('./DATA.json');
 let html = '';
 data.restaurants.forEach((restaurant) => {
   html += `
-    <div class="resto">
-    <h3 class="location-tag">${restaurant.city}</h3>
-    <div class="foto">
+    <div class="resto-item">
+    <h3 class="resto-item__location-tag">${restaurant.city}</h3>
+    <div class="resto-item__photo">
         <img
         src=${restaurant.pictureId}
         alt="${restaurant.name}}"
         />
     </div>
-    <p class="rating">Rating: ${restaurant.rating}</p>
+    <p class="resto-item__rate">Rating: ${restaurant.rating}</p>
     <div class="content">
-        <h3 class="resto-title"><a href="#">${restaurant.name}</a></h3>
-        <p class="resto-desc">
+        <h3 class="resto-item__title"><a href="#">${restaurant.name}</a></h3>
+        <p class="resto-item__desc">
         ${restaurant.description}
         </p>
     </div>
     </div>      
   `;
-  document.querySelector('#restos').innerHTML = html;
+  document.querySelector('#restaurant').innerHTML = html;
 });
